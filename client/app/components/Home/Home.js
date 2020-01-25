7ab412b7c2a914da7d7b390f066bf38038d718d9
Daniel Andrade <dcca12@gmail.com>
2020-01-25 14:50:49 -0800

    render bus stops of browser location

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,51 +1,81 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
   const [coordinates, setCoordinates] = useState({
     lat: 0,
-    long: 0,
+    long: 0
   });
   const [isLoading, setIsLoading] = useState(false);
+  const [busStops, setBusStops] = useState({});
 
   const fetchCoordinatesHandler = async () => {
     setIsLoading(true);
     try {
-        const { coords } = await getCurrentPosition();
-        const { latitude, longitude } = coords;
-        // Handle coordinates
-        setCoordinates({
-          lat: latitude,
-          long: longitude,
-        })
-        // Loading
-        setIsLoading(false);
-
+      const { coords } = await getCurrentPosition();
+      const { latitude, longitude } = coords;
+      // Handle coordinates
+      setCoordinates({
+        lat: latitude.toFixed(5),
+        long: longitude.toFixed(5)
+      });
+      // Loading
+      setIsLoading(false);
     } catch (error) {
-        // Handle error
-        console.error(error);
-        setIsLoading(false);
+      // Handle error
+      console.error(error);
+      setIsLoading(false);
     }
-  }
+  };
 
-  function getCurrentPosition(options = {}) {
+  function getCurrentPosition() {
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      navigator.geolocation.getCurrentPosition(resolve, reject, {
+        enableHighAccuracy: true
+      });
     });
   }
 
-  if(isLoading){
-    return (
-      <p>Loading...</p>
-    )
+  const fetchBusStops = () => {
+    setIsLoading(true);
+    // ?id=" + props.match.params.id)
+    fetch("/get-bus-stops?lat=" + coordinates.lat + "&long=" + coordinates.long)
+      // Runs when success
+      .then(response => response.json())
+      .then(response => {
+        setIsLoading(false);
+        console.log(response);
+        setBusStops(response);
+      })
+      // Runs when error
+      .catch(error => {
+        setIsLoading(false);
+        setErrorList(error);
+      });
+  };
+
+  // Handle loading
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
 
   return (
     <div>
-      <button onClick={fetchCoordinatesHandler} >Get Bus Stops</button>
-      <p>Latitude: {coordinates.lat} Longitude: {coordinates.long}</p>
+      <button onClick={fetchBusStops}>API Test</button>
+      <button onClick={fetchCoordinatesHandler}>Get Bus Stops</button>
+      <p>
+        Latitude: {coordinates.lat} Longitude: {coordinates.long}
+      </p>
+      {Object.keys(busStops).map(keys => {
+        console.log(busStops[keys].StopNo);
+        return (
+          <div key={busStops}>
+            <h1>Name: {busStops[keys].Name}</h1>
+            <p>On Street: {busStops[keys].OnStreet}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,40 @@
-const fs = require('fs');
-const path = require('path');
+const API_KEY = process.env.API_KEY;
+const request = require("request");
 
-module.exports = (app) => {
-  // API routes
-  fs.readdirSync(__dirname + '/api/').forEach((file) => {
-    require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
+const options = {};
+
+module.exports = app => {
+  // API Routes
+  // app.get("/get-bus-stops", (req, res) => {
+  //   let urlApi =
+  //     "https://api.translink.ca/rttiapi/v1/stops?apikey=" +
+  //     API_KEY +
+  //     "&lat=49.2344&long=-123.1451";
+  //   fetch(urlApi)
+  //     .then(res => res.send(res.json()))
+  //     .then();
+  // });
+  app.get("/get-bus-stops", (req, res) => {
+    let lat = req.query.lat;
+    let long = req.query.long;
+    const urlApi =
+      "https://api.translink.ca/rttiapi/v1/stops?apikey=" +
+      API_KEY +
+      "&lat=" +
+      lat +
+      "&long=" +
+      long;
+    let options = {
+      url: urlApi,
+      headers: {
+        "content-type": "application/JSON"
+      }
+    };
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        let jData = JSON.parse(body);
+        res.send(jData);
+      }
+    });
   });
 };
