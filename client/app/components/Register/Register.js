aa769ec7b191720121ddafe04ba5b6948675798e
DCCA <dcca12@gmail.com>
2020-01-28 15:21:22 -0800

    add basic styles to register page

--- client/app/components/Register/Register.js
+++ client/app/components/Register/Register.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React from 'react';
 
 export default function Register() {
   return (
-    <div>
-      <form className="flex flex-column justify-center align-center">
-        <label>
+    <div className='m3 border rounded p1'>
+      <h2 className='p1 center mb2'>Register</h2>
+      <form className='flex flex-column justify-center align-center'>
+        <label className='p1'>
           E-mail:
-          <input name="email" type="email" placeholder="e-mail"></input>
+          <input name='email' type='email' placeholder='e-mail'></input>
         </label>
-        <labe>
+        <label className='p1'>
           Password:
-          <input name="password" type="password" placeholder="password"></input>
-        </labe>
-        <button>Create Account</button>
+          <input name='password' type='password' placeholder='password'></input>
+        </label>
+        <button className='mt2 p2 rounded' formAction='submit'>
+          Create Account
+        </button>
       </form>
     </div>
   );
